4d7f8b5aa7a7ec24ea6df0000d338f68b16836b4
Ivan <arturarutiunian@iMac.local>
2022-07-26 23:21:21 +0300

    app added...

---
+++ ReverseWords(1)/ReverseWords(1)/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ReverseWords(1)
+//
+//  Created by Ivan Pestov on 26.07.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        let vc = ViewController()
+        let navController = UINavigationController(rootViewController: vc)
+        window?.rootViewController = navController
+        return true
+    }
+    
+}
+
---
+++ ReverseWords(1)/ReverseWords(1)/ViewController.swift
@@ -0,0 +1,214 @@
+//
+//  ViewController.swift
+//  ReverseWords(1)
+//
+//  Created by Ivan Pestov on 26.07.2022.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    let titleLabel = UILabel()
+    let bodyLabel = UILabel()
+    let textField = UITextField()
+    let dividerView = UIView()
+    let scrollView = UIScrollView()
+    let reversedTextLabel = UILabel()
+    let button = UIButton()
+    
+    var tapped: Bool = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+        setupNavBar()
+    }
+}
+
+extension ViewController {
+    
+    private func style() {
+        
+        // Title Label
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.font = UIFont.systemFont(ofSize: 34, weight: .bold)
+        titleLabel.adjustsFontForContentSizeCategory = true // A Boolean that indicates whether the object automatically updates its font when titleLabelitleevice's content size category changes.
+        titleLabel.text = "Reverse Words"
+        titleLabel.textColor = .black
+        
+        
+        // Body Label
+        bodyLabel.translatesAutoresizingMaskIntoConstraints = false
+        bodyLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        bodyLabel.text = "This application will reverse your words. Please type text below"
+        bodyLabel.adjustsFontSizeToFitWidth = true
+        bodyLabel.numberOfLines = 0
+        bodyLabel.textColor = .gray
+        bodyLabel.textAlignment = .center
+        
+        
+        // Text Field
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Text to reverse"
+        textField.backgroundColor = .systemBackground
+        textField.delegate = self
+        
+        
+        // Divider View
+        dividerView.translatesAutoresizingMaskIntoConstraints = false
+        dividerView.backgroundColor = .separator
+        
+        
+        // Reversed Text Label
+        reversedTextLabel.translatesAutoresizingMaskIntoConstraints = false
+        reversedTextLabel.font = UIFont.systemFont(ofSize: 24, weight: .light)
+        reversedTextLabel.numberOfLines = 1
+        reversedTextLabel.textColor = .systemBlue
+        
+        // Scroll View
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.autoresizingMask = .flexibleWidth
+        scrollView.backgroundColor = .green
+        scrollView.alwaysBounceHorizontal = true
+        
+        
+        // Button
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 14
+        button.setTitle("Reverse", for: .normal)
+        button.titleLabel?.textColor = .white
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        
+        
+    }
+    
+    private func layout() {
+        
+        view.addSubview(titleLabel)
+        view.addSubview(bodyLabel)
+        view.addSubview(textField)
+        view.addSubview(dividerView)
+        view.addSubview(scrollView)
+        scrollView.addSubview(reversedTextLabel)
+        view.addSubview(button)
+        
+        
+        NSLayoutConstraint.activate([
+            
+            // Title Label
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 64),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
+            
+            // Body Label
+            bodyLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
+            bodyLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 33),
+            bodyLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -34),
+            
+            // Text Field
+            textField.topAnchor.constraint(equalTo: bodyLabel.bottomAnchor, constant: 40),
+            textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            view.trailingAnchor.constraint(equalTo: textField.trailingAnchor, constant: 8),
+            textField.heightAnchor.constraint(equalToConstant: 60),
+            
+            // Divider View
+            dividerView.leadingAnchor.constraint(equalTo: textField.leadingAnchor),
+            dividerView.trailingAnchor.constraint(equalTo: textField.trailingAnchor),
+            dividerView.heightAnchor.constraint(equalToConstant: 1),
+            dividerView.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 0),
+            
+            // Scroll View
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 364),
+            scrollView.leftAnchor.constraint(equalTo: textField.leftAnchor, constant: 0),
+            scrollView.rightAnchor.constraint(equalTo: textField.rightAnchor, constant: 0),
+            scrollView.bottomAnchor.constraint(equalTo: view.topAnchor, constant: -0),
+            scrollView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            scrollView.heightAnchor.constraint(equalTo: textField.heightAnchor),
+            
+            // Button
+            button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -66),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
+            button.widthAnchor.constraint(equalToConstant: 343),
+            button.heightAnchor.constraint(equalToConstant: 66),
+        ])
+    }
+    
+    private func setupNavBar() {
+        title = "Reverse Words"
+    }
+    
+}
+
+extension ViewController {
+    
+    @objc func buttonTapped() {
+        
+        reversedTextLabel.text = reverseText()
+        
+        if tapped {
+            button.setTitle("Reverse", for: .normal)
+            reversedTextLabel.text = ""
+        } else {
+            button.setTitle("Clear", for: .normal)
+            dividerView.backgroundColor = .separator
+        }
+        tapped.toggle()
+    }
+    
+    func reverseText() -> String {
+        let string = textField.text
+        let reversed = String(string!.reversed())
+        
+        return reversed
+    }
+    
+}
+
+// MARK: - UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    
+    // return NO to disallow editing.
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    // became first responder
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+    }
+    
+    // return YES to allow editing to stop and to resign first responder status.
+    // return NO to disallow the editing session to end
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        dividerView.backgroundColor = .separator
+        return true
+    }
+    
+    // if implemented, called in place of textFieldDidEndEditing: ?
+    func textFieldDidEndEditing(_ textField: UITextField) {
+    }
+    
+    // detect - keypress
+    // return NO to not change text
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        dividerView.backgroundColor = .systemBlue
+        return true
+    }
+    
+    // called when 'clear' button pressed. return NO to ignore (no notifications)
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    // called when 'return' key pressed. return NO to ignore.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.endEditing(true) // resign first responder
+        return true
+    }
+}
+
+
+
+
